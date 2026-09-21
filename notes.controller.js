a6488bddf29e66ac30245791c291f89bbb07c62f
Ivan Shevchenko <vedagorhors777@yandex.ru>
2026-09-21 16:23:28 +0300

    Web-server

--- notes.controller.js
+++ notes.controller.js
@@ -44,29 +44,24 @@ async function removeNote(id) {
   console.log(chalk.red(`Note with id="${id}" has been removed.`));
 }
 
-// теперь добавим редактирование. Передадим id заметки и её новое название
 async function updateNote(id, title) {
-  // сначала прочитаем все заметки, а затем найдем нужную по id
   const notes = await getNotes();
   const note = notes.find((note) => note.id === id);
 
-  // если заметки уже нет, вернем null. Сервер проверит это и отправит ошибку 404
   if (!note) {
     return null;
   }
 
-  // find вернул сам объект из массива, поэтому здесь меняем название и в массиве notes. id оставляем прежним
   note.title = title;
-  // ждем сохранения в db.json, чтобы отправить браузеру результат только после записи
+
   await saveNotes(notes);
   return note;
 }
 
 module.exports = {
-  // addNote, printNotes, removeNote
   addNote,
   getNotes,
   removeNote,
-  // экспортируем функцию, чтобы использовать её в index.js
+
   updateNote,
 };
